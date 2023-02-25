13c32fcb1bee27b66729545dbdd59b3ccf268623
Kibet Kipkemoi <k.kipkemoi@locomogo.com>
2023-02-25 12:35:44 +0100

    Trigger a buzzer at given interval times
    Chose a generate interval function and implemented a function to trigger the buzzer at the given interval times.

--- learning_buzzer.py
+++ learning_buzzer.py
@@ -15,28 +15,11 @@ def buzzer():
     """The buzzer / sound that will go off every few minutes"""
     playsound(SOUND_FILE)
     print("Buzzing") # TO DO: replace with a buzzer counter or something to show that program is alive incase user's sound is off
-"""
-def generate_interval_times(x, y, z):
-    intervals = []
-    min_time = 0
-    y = y * 60 # Convert minutes to seconds
 
-    for i in range(x):
-        max_time = y - (x - i - 1) * z
-        if min_time >= max_time:
-            break
-        interval_time = random.uniform(min_time, max_time)
-        intervals.append(interval_time)
-        min_time = interval_time + z
-    return intervals
-"""
-
-def generate_interval_times(y, z):
-    """
-    Returns a list of x interval times (in seconds) between 0 and y minutes, with a minimum distance of z seconds between each interval time.
-    """
-    interval_amount = int(y / 2)  # calculate the number of intervals to generate
-    #interval_amount += random.randint(-0.2*interval_amount, 0.2*interval_amount) # add a random amount of intervals to the base amount
+def generate_interval_times(duration, z):
+    """Generate a list of interval times in seconds"""
+    interval_amount = int(duration / 3)  # calculate the number of intervals to generate
+    interval_amount += random.randint(int(-0.2*interval_amount), int(0.2*interval_amount)) # add a random amount of intervals to the base amount
     min_interval_time = 120   
     max_interval_time = 180  
     intervals = []
@@ -47,55 +30,25 @@ def generate_interval_times(y, z):
         min_interval_time = interval_time + z
         max_interval_time = min_interval_time + random.uniform(min_interval_time, max_interval_time)
     
-    return [t * 60 for t in intervals]  # convert intervals to seconds and return as list
-
-"""
-def generate_interval_times(duration, z):
-    intervals = []
-    x = int(duration/2) # The amount of intervals
-    #x = 10 # The amount of intervals
-    min_time = 0
-
-    for i in range(x):
-        max_time = duration - (x - i - 1) * z
-        if min_time >= max_time:
-            break
-        interval_time = random.uniform(min_time, max_time)
-        intervals.append(interval_time)
-        min_time = interval_time + z
-    return intervals
-"""
-
-def interval_times(duration, min_interval_size, recursion_times):
-    """The time intervals that the buzzer will go off"""
-    interval_amount = int(duration / 2) # An average of one interval per two minutes
-    intervals = np.random.uniform(0, duration, interval_amount)
-    intervals = np.sort(intervals)
-    if np.all(np.diff(intervals) >= min_interval_size):
-        print(f"Recursions: {recursion_times}") # TO DO: remove this line later, when done with testing
-        return intervals
-    else:
-        return interval_times(duration, min_interval_size, recursion_times+1)
+    return intervals  # convert intervals to seconds and return as list
 
 def interval_buzzer(intervals):
     """The buzzer that will go off at the intervals"""
-    interval_iter = iter(intervals)
-    for interval in interval_iter:
-        pass
-        #print(f"interval: {interval} seconds")
-        #next_interval = next(interval_iter)
-        #print(f"next interval: {next_interval} seconds")
-        #time.sleep(next_interval-interval)
-        #start_time = time.time()
-        #buzzer()
-        #end_time = time.time()
-        #print(f"Time taken: {end_time-start_time} seconds")
+    time_start = time.time()
+    time_end = time_start + (DURATION * 60)
+    i = 0
+    while((time.time() < time_end) and (i < len(intervals))):
+        if((time.time() - time_start <= intervals[i] + 1) and (time.time() - time_start >= intervals[i])):
+            print(f"The time is: {time.time() - time_start}")
+            temp_time_start = time.time()
+            buzzer()
+            temp_time_end = time.time()
+            print(f"Time taken to buzz: {temp_time_end - temp_time_start}")
+            i += 1
 
 def main():
     intervals = generate_interval_times(DURATION, MIN_INTERVAL_SIZE)
-    #intervals = interval_times(DURATION, MIN_INTERVAL_SIZE, RECURSION_LOG)
-    #buzzer()
     print(intervals)
-   # interval_buzzer(intervals)
+    interval_buzzer(intervals)
 
 main()
